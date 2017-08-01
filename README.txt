bbf3814100f61fb733e560eaa332d08ca9f5f917
Luiz (Harry) Guilherme de Paula Santos <luiz.paula@embrear.com.br>
2017-08-01 16:24:54 -0300

    First commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/harrysystems/pen/fcab68bbc023b51e42756aa8d2e706c2.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*
+	it differs from icono.css because it's size is always the same
+	instead of needing to wrap it inside a div
+	
+	- box does not depend on margins
+	- size should be responsive to the font-size
+	- colored icons (for programming languages and flags)
+	
+	
+	reference:
+	- cssicon.space
+	- git.io/icono
+	- fontawesome.io/icons
+	- useiconic.com/icons/
+	
+	problems:
+	- pixalization and spaces between pixels due to fractions
+*/
